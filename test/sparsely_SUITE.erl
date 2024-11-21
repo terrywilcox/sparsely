309b9f44f0bb037d290e5d47355d8c468ca1adf9
Terry Wilcox <terry.wilcox@mac.com>
2024-11-20 18:06:03 -0700

    Fix copyright year and ignore .DS_Store

--- test/sparsely_SUITE.erl
+++ test/sparsely_SUITE.erl
@@ -1,4 +1,4 @@
-%% Copyright 2022 Terry Wilcox
+%% Copyright 2024 Terry Wilcox
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
