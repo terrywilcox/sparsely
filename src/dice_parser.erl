cc6168ff36837481d05b428a017287fe94652774
Terry Wilcox <terry.wilcox@mac.com>
2025-01-09 09:41:50 -0700

    Add explode to dice parsing
    Dice parsing was lacking explosions, so added them.
    This revealed a bug in the number parsing if the parsed
    value was empty.
    Also reformatted code nicely.

--- src/dice_parser.erl
+++ src/dice_parser.erl
@@ -1,4 +1,4 @@
-%% Copyright 2024 Terry Wilcox 
+%% Copyright 2024 Terry Wilcox
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -14,11 +14,14 @@
 
 -module(dice_parser).
 
--export[digit/0, digits/0, pos_int/0, dice/0, number_of_dice/0].
--export[integer/0, float/0, operator/0, number/0].
--export[dice_term/0, variable/0, term/0, expression/0].
--export[expression_parse/1, pos_int_or_variable/0].
--export[number_or_variable/0].
+-export([digit/0, digits/0, pos_int/0, dice/0, number_of_dice/0]).
+-export([integer/0, float/0, operator/0, number/0]).
+-export([dice_term/0, variable/0, expression_factor/0, expression/0]).
+-export([expression_parse/1, dice_const/0, expression_term/0]).
+-export([number_or_variable/0, explode/0]).
+-export([expression_term_parse/1, expression_factor_parse/1]).
+-export([bracketed_expression_parse/1]).
+-export([parser/0, dice_wrapper/1]).
 
 %% Simple dice notation takes the form of NdS, where N is the number of dice and
 %% S is the size of dice. So 3d6 is 3 six-sided dice. The 'd' can also be a 'D'.
@@ -29,147 +32,321 @@ convert_to_fractional_float(Int) when is_integer(Int) ->
     Int / math:pow(10, Len).
 
 white_space() ->
-	sparsely:optional(sparsely:repeat(sparsely:character(" \t\n")), skip).
+    sparsely:skip(
+        sparsely:repeat(
+            sparsely:character(" \t\n"))).
+
+additive_operator() ->
+    sparsely:wrap(
+        sparsely:character("+-"),
+        fun ({ok, $+, Rest}) ->
+                {ok, add, Rest};
+            ({ok, $-, Rest}) ->
+                {ok, subtract, Rest};
+            ({error, _} = Error) ->
+                Error
+        end).
+
+multiplicative_operator() ->
+    sparsely:wrap(
+        sparsely:character("*/"),
+        fun ({ok, $*, Rest}) ->
+                {ok, multiply, Rest};
+            ({ok, $/, Rest}) ->
+                {ok, divide, Rest};
+            ({error, _} = Error) ->
+                Error
+        end).
 
 operator() ->
-	sparsely:character("+-*/").
+    sparsely:one_of([additive_operator(), multiplicative_operator()]).
 
 digit() ->
-	sparsely:character("1234567890").
+    sparsely:character("1234567890").
 
 digits() ->
-	sparsely:repeat(digit()).
+    sparsely:repeat(digit()).
 
 alpha() ->
-	sparsely:character("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").
+    sparsely:character("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").
 
 pos_int() ->
-	sparsely:wrap(digits(),
-		      fun({ok, Value, Rest}) -> {Integer, _} = string:to_integer(Value),
-						{ok, Integer, Rest};
-			 (Any) -> Any
-		      end).
+    sparsely:wrap(digits(),
+                  fun ({error, _} = Error) ->
+                          Error;
+                      ({ok, Value, Rest}) ->
+                          case string:to_integer(Value) of
+                              {error, _} ->
+                                  {error, "Not a valid integer"};
+                              {Integer, _} ->
+                                  {ok, Integer, Rest}
+                          end
+                  end).
 
 variable() ->
-	VariableName = sparsely:repeat(alpha()),
-	VariableToken = sparsely:character("$"),
-	VariableParser = sparsely:chain([VariableToken, sparsely:character("{"),
-					 VariableName, sparsely:character("}")]),
-	sparsely:wrap(VariableParser,
-		      fun({ok, [_, _, Name, _], Rest}) -> {ok, {variable, Name}, Rest};
-			 (Any) -> Any
-		      end).
+    VariableName = sparsely:repeat(alpha()),
+    VariableToken = sparsely:character("$"),
+    VariableParser =
+        sparsely:chain([VariableToken,
+                        sparsely:character("{"),
+                        VariableName,
+                        sparsely:character("}")]),
+    sparsely:wrap(VariableParser,
+                  fun ({ok, [_, _, Name, _], Rest}) ->
+                          {ok, {variable, Name}, Rest};
+                      ({error, _} = Error) ->
+                          Error
+                  end).
 
 integer() ->
-	Negative = sparsely:character("-"),
-	sparsely:wrap(sparsely:chain([sparsely:optional(Negative, 1), pos_int()]),
-		      fun({ok, [Sign, Value], Rest}) ->
-				      case Sign of
-					  $- -> {ok, -Value, Rest};
-					  _ -> {ok, Value, Rest}
-				      end;
-			 (Any) -> Any
-		      end).
+    Negative = sparsely:character("-"),
+    sparsely:wrap(
+        sparsely:chain([sparsely:optional(Negative, 1), pos_int()]),
+        fun ({ok, [Sign, Value], Rest}) ->
+                case Sign of
+                    $- ->
+                        {ok, -Value, Rest};
+                    _ ->
+                        {ok, Value, Rest}
+                end;
+            ({error, _} = Error) ->
+                Error
+        end).
 
 float() ->
-	sparsely:wrap(sparsely:chain([integer(), sparsely:character("."), pos_int()]),
-		      fun({ok, [Value, _, Fraction], Rest}) ->
-				      case (Value < 0) == true of
-					      true -> {ok, Value - convert_to_fractional_float(Fraction), Rest};
-					      _ -> {ok, Value + convert_to_fractional_float(Fraction), Rest}
-				      end;
-			 (Any) -> Any
-		      end).
+    sparsely:wrap(
+        sparsely:chain([integer(), sparsely:character("."), pos_int()]),
+        fun ({ok, [Value, _, Fraction], Rest}) ->
+                case (Value < 0) == true of
+                    true ->
+                        {ok, Value - convert_to_fractional_float(Fraction), Rest};
+                    _ ->
+                        {ok, Value + convert_to_fractional_float(Fraction), Rest}
+                end;
+            ({error, _} = Error) ->
+                Error
+        end).
 
 number() ->
-	sparsely:one_of([float(), integer()]).
+    sparsely:one_of([float(), integer()]).
 
-dice() ->	
-	D = sparsely:character("dD"),
-	sparsely:wrap(D, fun({ok, _, Rest}) -> {ok, dice, Rest};
-			    (Any) -> Any
-			 end).
+const() ->
+    sparsely:one_of([float(), integer(), variable()]).
 
-pos_int_or_variable() ->
-	sparsely:one_of([variable(), pos_int()]).
+dice() ->
+    D = sparsely:character("dD"),
+    sparsely:wrap(D,
+                  fun ({ok, _, Rest}) ->
+                          {ok, dice, Rest};
+                      ({error, _} = Error) ->
+                          Error
+                  end).
+
+dice_const() ->
+    sparsely:one_of([variable(), pos_int()]).
 
 number_or_variable() ->
-	sparsely:one_of([variable(), number()]).
+    sparsely:one_of([variable(), number()]).
 
 number_of_dice() ->
-	sparsely:optional(pos_int_or_variable(), 1).
+    sparsely:optional(dice_const(), 1).
+
+explode() ->
+    ExplodePoint = sparsely:optional(pos_int()),
+    Explode = sparsely:character("!"),
+    ExplodeWrap =
+        sparsely:wrap(Explode,
+                      fun ({ok, _, Rest}) ->
+                              {ok, explode, Rest};
+                          ({error, _} = Error) ->
+                              Error
+                      end),
+
+    ExplodeChain = sparsely:chain([ExplodeWrap, ExplodePoint]),
+
+    sparsely:optional(ExplodeChain).
 
 keep_dice() ->
-	Keep = sparsely:wrap(sparsely:character("kK"),
-			     fun({ok, _, Rest}) -> {ok, keep, Rest};
-				(Any) -> Any
-			     end),
+    Keep =
+        sparsely:wrap(
+            sparsely:character("kK"),
+            fun ({ok, _, Rest}) ->
+                    {ok, keep, Rest};
+                ({error, _} = Error) ->
+                    Error
+            end),
 
-	Highest = sparsely:wrap(sparsely:character("hH"),
-				fun({ok, _, Rest}) -> {ok, highest, Rest};
-				   (Any) -> Any
-				end),
+    Drop =
+        sparsely:wrap(
+            sparsely:character("dD"),
+            fun ({ok, _, Rest}) ->
+                    {ok, drop, Rest};
+                ({error, _} = Error) ->
+                    Error
+            end),
 
-	Lowest = sparsely:wrap(sparsely:character("lL"),
-			       fun({ok, _, Rest}) -> {ok, lowest, Rest};
-				  (Any) -> Any
-			       end),
+    Highest =
+        sparsely:wrap(
+            sparsely:character("hH"),
+            fun ({ok, _, Rest}) ->
+                    {ok, highest, Rest};
+                ({error, _} = Error) ->
+                    Error
+            end),
 
-	NumberToKeep = sparsely:optional(pos_int_or_variable(), 1),
+    Lowest =
+        sparsely:wrap(
+            sparsely:character("lL"),
+            fun ({ok, _, Rest}) ->
+                    {ok, lowest, Rest};
+                ({error, _} = Error) ->
+                    Error
+            end),
 
-	sparsely:optional(
-		   sparsely:chain(
-		     [Keep, sparsely:optional(
-			      sparsely:one_of([Highest, Lowest]), highest),
-		      NumberToKeep]),
-		   ignore).
+    NumberToKeep = sparsely:optional(dice_const(), 1),
+
+    KeepDice =
+        sparsely:chain([sparsely:one_of([Keep, Drop]),
+                        sparsely:optional(
+                            sparsely:one_of([Highest, Lowest]), highest),
+                        NumberToKeep]),
+    sparsely:optional(KeepDice).
+
+dice_roll() ->
+    sparsely:chain([number_of_dice(), dice(), dice_const()]).
 
 dice_term() ->
-	Roll = sparsely:chain([number_of_dice(), dice(), pos_int_or_variable(), keep_dice()]),
-	sparsely:wrap(Roll,
-		      fun({ok, [NumberOfDice, dice, SidesOfDice], Rest}) ->
-				      {ok, {dice, {NumberOfDice, SidesOfDice}}, Rest};
-		         ({ok, [NumberOfDice, dice, SidesOfDice, [keep, Parity, KeepNumber]], Rest}) ->
-				      {ok, {dice, {NumberOfDice, SidesOfDice}, {keep, {Parity, KeepNumber}}}, Rest};
-			 (Any) -> Any
-		      end).
+    Roll = sparsely:chain([dice_roll(), explode(), keep_dice()]),
+    F = fun dice_parser:dice_wrapper/1,
+    sparsely:wrap(Roll, F).
 
-term() ->
-	sparsely:one_of([expression(), dice_term(), number_or_variable()]).
+dice_wrapper({error, _} = Error) ->
+    Error;
+dice_wrapper([{error, _} | _] = Error) ->
+    Error;
+dice_wrapper([{ok, DiceResult, Rest}]) ->
+    [{ok, dice_wrapper(DiceResult, []), Rest}];
+dice_wrapper({ok, DiceResult, Rest}) ->
+    {ok, dice_wrapper(DiceResult, []), Rest}.
+
+dice_wrapper([], Acc) ->
+    Dice = proplists:get_value(dice, Acc),
+    Roll = {dice, Dice},
+    Roll2 =
+        case proplists:get_value(explode, Acc, undefined) of
+            undefined ->
+                Roll;
+            Point ->
+                {explode, {Point, Roll}}
+        end,
+    case proplists:get_value(keep, Acc, undefined) of
+        undefined ->
+            Roll2;
+        {Keep, Parity, Number} ->
+            {Keep, {Parity, Number}, Roll2}
+    end;
+dice_wrapper([[NumberOfDice, dice, SidesOfDice] | Rest], Acc) ->
+    dice_wrapper(Rest, [{dice, {NumberOfDice, SidesOfDice}} | Acc]);
+dice_wrapper([[explode, Point] | Rest], Acc) ->
+    dice_wrapper(Rest, [{explode, Point} | Acc]);
+dice_wrapper([[explode] | Rest], Acc) ->
+    dice_wrapper(Rest, [{explode, maximum} | Acc]);
+dice_wrapper([[Keep, Parity, Number] | Rest], Acc) when Keep == keep; Keep == drop ->
+    dice_wrapper(Rest, [{keep, {Keep, Parity, Number}} | Acc]).
+
+%% expression_factor is an expression in brackets, a dice_term, or a const
+%% expression_term is an expression_factor followed by zero or more pairs of a multiplicative_operator and another expression_factor
+%% expression is an expression_term followed by zero or more pairs of additive_operator and another expression_term
+
+bracketed_expression_parse(S) ->
+    LeftBracket = sparsely:chain([white_space(), sparsely:character("("), white_space()]),
+    RightBracket =
+        sparsely:ignore(
+            sparsely:chain([white_space(), sparsely:character(")"), white_space()])),
+    Expression = expression(),
+
+    InnerExpression =
+        case LeftBracket(S) of
+            {ok, _, Rest} ->
+                case Expression(Rest) of
+                    {error, _} = Error ->
+                        Error;
+                    {ok, ExpressionValue, Rest2} ->
+                        case RightBracket(Rest2) of
+                            {error, _} ->
+                                {error, "No right bracket"};
+                            {ok, _, Rest3} ->
+                                {ok, ExpressionValue, Rest3}
+                        end
+                end;
+            {error, _} = Error ->
+                Error
+        end,
+    InnerExpression.
+
+bracketed_expression() ->
+    sparsely:parse_fun(dice_parser, bracketed_expression_parse).
+
+expression_factor_parse(S) ->
+    ExpressionFactor = sparsely:one_of([bracketed_expression(), dice_term(), const()]),
+    ExpressionFactor(S).
+
+expression_factor() ->
+    sparsely:parse_fun(dice_parser, expression_factor_parse).
+
+expression_term_parse(S) ->
+    ExpressionFactor = expression_factor(),
+    MultiplicativeOperator = multiplicative_operator(),
+
+    Term =
+        case ExpressionFactor(S) of
+            {error, _} = Error ->
+                Error;
+            {ok, Left, Rest} ->
+                case MultiplicativeOperator(Rest) of
+                    {error, _} ->
+                        {ok, Left, Rest};
+                    {ok, Operator, Rest2} ->
+                        case expression_term_parse(Rest2) of
+                            {error, _} = Error ->
+                                Error;
+                            {ok, Right, Rest3} ->
+                                {ok, {Operator, Left, Right}, Rest3}
+                        end
+                end
+        end,
+
+    Term.
+
+expression_term() ->
+    sparsely:parse_fun(dice_parser, expression_term_parse).
 
 expression_parse(S) ->
-	LeftBracket = sparsely:character("("),
-	RightBracket = sparsely:character(")"),
-	WhiteSpace = white_space(),
-	Expression = sparsely:chain([WhiteSpace, LeftBracket,
-				     WhiteSpace, term(),
-				     WhiteSpace, operator(),
-				     WhiteSpace, term(),
-				     WhiteSpace, RightBracket,
-				     WhiteSpace
-				    ]),
+    ExpressionTerm = expression_term(),
+    AdditiveOperator = additive_operator(),
 
-	Wrapped = sparsely:wrap(Expression,
-				fun({ok, [$(, Left, Op, Right, $)], Rest}) ->
-						{ok, {Op, Left, Right}, Rest};
-				   (Any) -> Any
-				end),
-	Wrapped(S).
+    Expression =
+        case ExpressionTerm(S) of
+            {error, _} = Error ->
+                Error;
+            {ok, Left, Rest} ->
+                case AdditiveOperator(Rest) of
+                    {error, _} ->
+                        {ok, Left, Rest};
+                    {ok, Operator, Rest2} ->
+                        case expression_parse(Rest2) of
+                            {error, _} = Error ->
+                                Error;
+                            {ok, Right, Rest3} ->
+                                {ok, {Operator, Left, Right}, Rest3}
+                        end
+                end
+        end,
+
+    Expression.
 
 expression() ->
-	% This requires some explanation. This parser could be defined
-	% in the same manner as the other parsers: as an anonymous function.
-	% But because a term can be an expression and an expression can
-	% contain terms, we have a circular dependency which results
-	% in a recursive definition. If we do this at parser/anonymous function
-	% creation time, we go into an infinite recursion loop.
-	% Instead we define the parser as a named function that, when called,
-	% creates a new parser that calls the named function recursively,
-	% avoiding the infinite recursion loop.
-	sparsely:parse_fun(dice_parser, expression_parse).
+    sparsely:parse_fun(dice_parser, expression_parse).
 
-
-
-
-
-
+parser() ->
+    expression().
--- test/dice_parser_SUITE.erl
+++ test/dice_parser_SUITE.erl
@@ -12,14 +12,14 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module (dice_parser_SUITE).
+-module(dice_parser_SUITE).
+
 -include_lib("eunit/include/eunit.hrl").
--include("include/sparsely.hrl").
 
--export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2, end_per_testcase/2]).
--export([single_die/1, multiple_dice/1, variable/1, number_of_dice/1, variable_sides/1]).
--export([integer/1, float/1, number/1, term/1, expression/1, keep/1]).
-
+-export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
+         end_per_testcase/2]).
+-export([variable/1, dice/1, explode/1]).
+-export([integer/1, float/1, number/1, expression/1, keep/1]).
 
 init_per_suite(Config) ->
     ct:pal("Initializing test suite...~n"),
@@ -39,73 +39,58 @@ end_per_testcase(TestCase, _Config) ->
 
 % Test cases
 all() ->
-    [
-     	integer, float, number, single_die, multiple_dice,
-	variable, number_of_dice, term, expression, variable_sides,
-	keep
-    ].
-
-%assertError(Error) ->
-%	?assertEqual({error, "no matching character"}, Error).
+    [integer, float, number, explode, variable, dice, expression, keep].
 
 integer(_Config) ->
-	Parser = dice_parser:integer(),
-	?assertEqual({ok, 300, <<>>}, Parser("300")),
-	?assertEqual({ok, -300, <<>>}, Parser("-300")).
+    Parser = dice_parser:parser(),
+    ?assertEqual({ok, 300, <<>>}, Parser("300")),
+    ?assertEqual({ok, -300, <<>>}, Parser("-300")).
 
 float(_Config) ->
-	Parser = dice_parser:float(),
-	?assertEqual({ok, 300.2, <<>>}, Parser("300.2")),
-	?assertEqual({ok, -300.2, <<>>}, Parser("-300.2")).
+    Parser = dice_parser:parser(),
+    ?assertEqual({ok, 300.2, <<>>}, Parser("300.2")),
+    ?assertEqual({ok, -300.2, <<>>}, Parser("-300.2")).
 
 number(_Config) ->
-	Parser = dice_parser:number(),
-	?assertEqual({ok, -10, <<"+">>}, Parser("-10+")),
-	?assertEqual({ok, 10.2, <<"bbb">>}, Parser("10.2bbb")),
-	?assertEqual({ok, -33.3, <<"   4">>}, Parser("-33.3   4")).
+    Parser = dice_parser:parser(),
+    ?assertEqual({ok, -10, <<>>}, Parser("-10")),
+    ?assertEqual({ok, -10, <<"t">>}, Parser("-10t")),
+    ?assertEqual({ok, 10.2, <<"bbb">>}, Parser("10.2bbb")),
+    ?assertEqual({ok, -33.3, <<"   4">>}, Parser("-33.3   4")).
 
-number_of_dice(_Config) ->
-	Parser = dice_parser:number_of_dice(),
-	?assertEqual({ok, 3, <<"d6">>}, Parser("3d6")),
-	% TODO: optional with default does not convert S to binary
-	?assertEqual({ok, 1, "d6"}, Parser("d6")),
-	?assertEqual({ok, {variable, "x"}, <<"d6">>}, Parser("${x}d6")),
-	?assertEqual({ok, 10, <<"d6">>}, Parser("10d6")).
+dice(_Config) ->
+    Parser = dice_parser:parser(),
+    ?assertEqual({ok, {dice, {3, 6}}, <<>>}, Parser("3d6")),
+    ?assertEqual({ok, {dice, {1, 6}}, <<>>}, Parser("d6")),
+    ?assertEqual({ok, {dice, {{variable, "x"}, {variable, "y"}}}, <<>>}, Parser("${x}d${y}")).
 
-single_die(_Config) ->
-	Parser = dice_parser:dice_term(),
-	?assertEqual({ok, {dice, {1, 6}}, <<>>}, Parser("d6")).
+explode(_Config) ->
+    Parser = dice_parser:explode(),
+    ?assertEqual({ok, [explode], <<"">>}, Parser("!")).
 
-multiple_dice(_Config) ->
-	Parser = dice_parser:dice_term(),
-	?assertEqual({ok, {dice, {10, 12}}, <<>>}, Parser("10d12")).
-
-variable_sides(_Config) ->
-	Parser = dice_parser:dice_term(),
-	?assertEqual({ok, {dice, {10, {variable, "x"}}}, <<>>}, Parser("10d${x}")).
+%        ?assertEqual({ok, [explode], <<"4">>}, Parser("!4")),
+%        ?assertEqual({ok, {explode, {dice, {{variable, "x"}, {variable, "y"}}}}, <<>>}, Parser("${x}d${y}!")).
 
 keep(_Config) ->
-	Parser = dice_parser:dice_term(),
-	?assertEqual({ok, {dice, {10, 6}, {keep, {highest, 1}}}, <<>>}, Parser("10d6kh1")),
-	?assertEqual({ok, {dice, {10, 6}, {keep, {highest, 1}}}, <<>>}, Parser("10d6k1")),
-	?assertEqual({ok, {dice, {10, 6}, {keep, {lowest, 3}}}, <<>>}, Parser("10d6kl3")).
+    Parser = dice_parser:parser(),
+    ?assertEqual({ok, {keep, {highest, 1}, {dice, {10, 6}}}, <<>>}, Parser("10d6kh1")),
+    ?assertEqual({ok, {keep, {highest, 1}, {dice, {10, 6}}}, <<>>}, Parser("10d6k1")),
+    ?assertEqual({ok, {keep, {lowest, 3}, {dice, {10, 6}}}, <<>>}, Parser("10d6kl3")),
+    ?assertEqual({ok, {drop, {lowest, 3}, {dice, {10, 6}}}, <<>>}, Parser("10d6dl3")),
+    ?assertEqual({ok, {drop, {lowest, 3}, {explode, {maximum, {dice, {10, 6}}}}}, <<>>},
+                 Parser("10d6!dl3")).
 
 variable(_Config) ->
-	Parser = dice_parser:variable(),
-	?assertEqual({ok, {variable, "CHA"}, <<"555">>}, Parser("${CHA}555")).
-
-term(_Config) ->
-	Parser = dice_parser:term(),
-	?assertEqual({ok, {dice, {10, 12}}, <<"bbb">>}, Parser("10d12bbb")),
-	?assertEqual({ok, {variable, "bob"}, <<>>}, Parser("${bob}")),
-	?assertEqual({ok, 1012, <<"bbb">>}, Parser("1012bbb")).
+    Parser = dice_parser:variable(),
+    ?assertEqual({ok, {variable, "CHA"}, <<"555">>}, Parser("${CHA}555")).
 
 expression(_Config) ->
-	Parser = dice_parser:expression(),
-	?assertEqual({ok, {$+, {dice, {10, 12}}, 3}, <<>>}, Parser("(10d12+3)")),
-	?assertEqual({ok, {$*, {variable, "bob"}, 7}, <<>>}, Parser("(${bob}*7)")),
-	?assertEqual({ok, {$-, {dice, {10, 12}}, {variable, "bbb"}}, <<>>}, Parser("(10d12-${bbb})")),
-	?assertEqual({ok, {$+, 42, {$-, {dice, {3, 12}}, {variable, "bbb"}}}, <<>>}, Parser("(42+(3d12-${bbb}))")),
-	?assertEqual({ok, {$+, 42, {$-, {dice, {{variable, "x"}, 12}}, {variable, "bbb"}}}, <<>>}, Parser("(42+(${x}d12-${bbb}))")),
-	?assertEqual({ok, {$+, 42, {$-, {dice, {{variable, "x"}, 12}}, {variable, "bbb"}}}, <<>>}, Parser("(42 + (${x}d12 - ${bbb}))")).
-
+    Parser = dice_parser:parser(),
+    ?assertEqual({ok, {dice, {3, 6}}, <<>>}, Parser("3d6")),
+    ?assertEqual({ok, {add, {dice, {3, 6}}, {multiply, 7, 4}}, <<>>}, Parser("3d6+7*4")),
+    ?assertEqual({ok, {add, {multiply, {dice, {3, 6}}, 7}, 4}, <<>>}, Parser("3d6*7+4")),
+    ?assertEqual({ok, {multiply, {dice, {3, 6}}, {add, 7, 4}}, <<>>}, Parser("3d6*(7+4)")),
+    ?assertEqual({ok, {divide, {add, 7, 5}, {subtract, 6, 3}}, <<>>},
+                 Parser("((7+5)/(6-3))")),
+    ?assertEqual({ok, {divide, {add, 7, 5}, {subtract, 6, {variable, "var"}}}, <<>>},
+                 Parser("((7+5)/(6-${var}))")).
