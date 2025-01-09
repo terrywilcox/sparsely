46dabe9e2b1769f60566bb469656fb9cef1815c7
Terry Wilcox <terry.wilcox@mac.com>
2025-01-09 09:27:59 -0700

    Add ignore parser
    Add a parser for ignoring results iff they exist.
    Also fix formatting, it was terrible.

--- src/sparsely.erl
+++ src/sparsely.erl
@@ -14,99 +14,123 @@
 
 -module(sparsely).
 
--export([one_of/1, chain/1, optional/1, optional/2, repeat/2, repeat/1,
-	 character/1, wrap/2, match/1, parse_fun/2, skip/1]).
+-export([one_of/1, chain/1, optional/1, optional/2, repeat/2, repeat/1, character/1,
+         wrap/2, match/1, parse_fun/2, skip/1, ignore/1]).
 
 wrap(Parser, F) when is_function(F) ->
-	fun(S) -> F(Parser(S)) end.
+    fun(S) -> F(Parser(S)) end.
 
 parse_fun(Module, Fun) ->
-	fun(S) -> F = fun Module:Fun/1,
-		  F(S)
-	end.
+    fun(S) ->
+       F = fun Module:Fun/1,
+       F(S)
+    end.
 
 character(ValidChars) when is_list(ValidChars) orelse is_binary(ValidChars) ->
-	BinChars = iolist_to_binary(ValidChars),
-	fun (<<>>) ->
-			{error, "no matching character"};
-	    ([]) ->
-			{error, "no matching character"};
-	    (S) ->
-			BinS = iolist_to_binary(S),
-			<<ParseChar, Rest/binary>> = BinS,
-			case binary:match(BinChars, <<ParseChar>>) of
-				{_, 1} -> {ok, ParseChar, Rest};
-				_ -> {error, "no matching character"}
-			end
-	end.
+    BinChars = iolist_to_binary(ValidChars),
+    fun (<<>>) ->
+            {error, "no matching character"};
+        ([]) ->
+            {error, "no matching character"};
+        (S) ->
+            BinS = iolist_to_binary(S),
+            <<ParseChar, Rest/binary>> = BinS,
+            case binary:match(BinChars, <<ParseChar>>) of
+                {_, 1} ->
+                    {ok, ParseChar, Rest};
+                _ ->
+                    {error, "no matching character"}
+            end
+    end.
 
 match(Chars) when is_list(Chars) ->
-	Parsers = [character([Char]) || Char <- Chars],
-	chain(Parsers).
+    Parsers = [character([Char]) || Char <- Chars],
+    chain(Parsers).
 
 repeat(Parser, Times) when is_integer(Times) ->
-	Parsers = [Parser || _ <- lists:seq(1, Times)],
-	chain(Parsers).
+    Parsers = [Parser || _ <- lists:seq(1, Times)],
+    chain(Parsers).
 
 repeat(Parser) ->
-	fun(S) -> repeat_parse(S, Parser, []) end.
+    fun(S) -> repeat_parse(S, Parser, []) end.
 
 repeat_parse(<<>>, _Parser, Acc) ->
-		{ok, lists:reverse(Acc), <<>>};
+    {ok, lists:reverse(Acc), <<>>};
+repeat_parse([], _Parser, Acc) ->
+    {ok, lists:reverse(Acc), <<>>};
 repeat_parse(S, Parser, Acc) ->
-	case Parser(S) of
-		{error, Reason} -> case length(Acc) of
-				      0 -> {error, Reason};
-				      _ ->{ok, lists:reverse(Acc), S}
-				   end;
-		{ok, ignore, Rest} -> repeat_parse(Rest, Parser, Acc);
-		{ok, Value, Rest} -> repeat_parse(Rest, Parser, [Value | Acc])
-	end.
+    case Parser(S) of
+        {error, Reason} ->
+            case length(Acc) of
+                0 ->
+                    {error, Reason};
+                _ ->
+                    {ok, lists:reverse(Acc), S}
+            end;
+        {ok, ignore, Rest} ->
+            repeat_parse(Rest, Parser, Acc);
+        {ok, Value, Rest} ->
+            repeat_parse(Rest, Parser, [Value | Acc])
+    end.
 
 one_of_parse(_S, []) ->
-	{error, "no matching character"};
-one_of_parse(S, [Parser| Parsers]) ->
-	case Parser(S) of
-		{error, _} -> one_of_parse(S, Parsers);
-		Any -> Any
-	end.
+    {error, "no matching character"};
+one_of_parse(S, [Parser | Parsers]) ->
+    case Parser(S) of
+        {ok, _, _} = Success ->
+            Success;
+        {error, _} ->
+            one_of_parse(S, Parsers)
+    end.
 
 one_of(Parsers) when is_list(Parsers) ->
-	fun(S) -> one_of_parse(S, Parsers) end.
+    fun(S) -> one_of_parse(S, Parsers) end.
 
 chain_parse(S, [], Acc) ->
-	{ok, lists:reverse(Acc), S};
-chain_parse(S, [Parser|Parsers], Acc) when is_function(Parser) ->
-	case Parser(S) of
-		{error, _} = Error -> Error;
-		{ok, ignore, Rest} -> chain_parse(Rest, Parsers, Acc);
-		{ok, Value, Rest} -> chain_parse(Rest, Parsers, [Value | Acc])
-	end.
+    {ok, lists:reverse(Acc), S};
+chain_parse(S, [Parser | Parsers], Acc) when is_function(Parser) ->
+    case Parser(S) of
+        {error, _} = Error ->
+            Error;
+        {ok, ignore, Rest} ->
+            chain_parse(Rest, Parsers, Acc);
+        {ok, Value, Rest} ->
+            chain_parse(Rest, Parsers, [Value | Acc])
+    end.
 
 chain(Parsers) when is_list(Parsers) ->
-	fun(S) -> chain_parse(S, Parsers, []) end.
+    fun(S) -> chain_parse(S, Parsers, []) end.
 
 optional(Parser) ->
-	optional(Parser, ignore).
+    optional(Parser, ignore).
 
 optional(Parser, skip) ->
-	fun(S) -> case Parser(S) of
-			  {error, _} -> {ok, ignore, S};
-			  {ok, _, Rest} -> {ok, ignore, Rest}
-		  end
-	end;
+    fun(S) ->
+       case Parser(S) of
+           {error, _} -> {ok, ignore, S};
+           {ok, _, Rest} -> {ok, ignore, Rest}
+       end
+    end;
 optional(Parser, Default) ->
-	fun(S) -> case Parser(S) of
-			  {error, _} -> {ok, Default, S};
-			  Any -> Any
-		  end
-	end.
+    fun(S) ->
+       case Parser(S) of
+           {error, _} -> {ok, Default, S};
+           {ok, _, _} = Success -> Success
+       end
+    end.
 
 skip(Parser) ->
-	fun(S) -> case Parser(S) of
-			  {error, _} -> {ok, ignore, S};
-			  {ok, _, Rest} -> {ok, ignore, Rest}
-		  end
-	end.
+    fun(S) ->
+       case Parser(S) of
+           {error, _} -> {ok, ignore, S};
+           {ok, _, Rest} -> {ok, ignore, Rest}
+       end
+    end.
 
-
+ignore(Parser) ->
+    fun(S) ->
+       case Parser(S) of
+           {error, _} = Error -> Error;
+           {ok, _, Rest} -> {ok, ignore, Rest}
+       end
+    end.
--- test/sparsely_SUITE.erl
+++ test/sparsely_SUITE.erl
@@ -12,16 +12,19 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module (sparsely_SUITE).
+-module(sparsely_SUITE).
+
 -include_lib("eunit/include/eunit.hrl").
+
 -include("include/sparsely.hrl").
 
--export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2, end_per_testcase/2]).
+-export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
+         end_per_testcase/2]).
 -export([parse_single_char/1, parse_char_from_set/1, parse_repeat/1]).
 -export([parse_one_of/1, parse_no_chars/1, parse_one_of_none/1, parse_match/1]).
 -export([parse_repeat_times/1, parse_chain/1, parse_optional/1, parse_wrap/1]).
 -export([parse_optional_with_default/1, parse_optional_with_long_default/1]).
--export([parse_fun/1, one_digit/1, parse_skip/1]).
+-export([parse_fun/1, one_digit/1, parse_skip/1, parse_ignore/1]).
 
 init_per_suite(Config) ->
     ct:pal("Initializing test suite...~n"),
@@ -41,189 +44,208 @@ end_per_testcase(TestCase, _Config) ->
 
 % Test cases
 all() ->
-    [
-        parse_single_char, parse_char_from_set, parse_repeat,
-	parse_one_of, parse_no_chars, parse_one_of_none,
-	parse_repeat_times, parse_chain, parse_optional,
-	parse_match, parse_wrap, parse_optional_with_default,
-	parse_optional_with_long_default, parse_fun,
-	parse_skip
-    ].
+    [parse_single_char,
+     parse_char_from_set,
+     parse_repeat,
+     parse_one_of,
+     parse_no_chars,
+     parse_one_of_none,
+     parse_repeat_times,
+     parse_chain,
+     parse_optional,
+     parse_match,
+     parse_wrap,
+     parse_optional_with_default,
+     parse_optional_with_long_default,
+     parse_fun,
+     parse_skip,
+     parse_ignore].
 
 assertError(Error) ->
-	?assertEqual({error, "no matching character"}, Error).
+    ?assertEqual({error, "no matching character"}, Error).
 
 parse_no_chars(_Config) ->
-	Parser = sparsely:character("a"),
-	assertError(Parser("")).
+    Parser = sparsely:character("a"),
+    assertError(Parser("")).
 
 parse_single_char(_Config) ->
-	Parser = sparsely:character("a"),
-	Result = Parser("ated"),
-	?assertEqual({ok, $a, <<"ted">>}, Result), 
+    Parser = sparsely:character("a"),
+    Result = Parser("ated"),
+    ?assertEqual({ok, $a, <<"ted">>}, Result),
 
-	assertError(Parser("zaz")).
+    assertError(Parser("zaz")).
 
 parse_char_from_set(_Config) ->
-	Parser = sparsely:character("abcde"),
-	Result = Parser("bted"),
-	?assertEqual({ok, $b, <<"ted">>}, Result), 
+    Parser = sparsely:character("abcde"),
+    Result = Parser("bted"),
+    ?assertEqual({ok, $b, <<"ted">>}, Result),
 
-	assertError(Parser("zaz")).
+    assertError(Parser("zaz")).
 
 parse_one_of(_Config) ->
-	Parser = sparsely:one_of([?DIGIT, ?LOWERCASE]),
+    Parser = sparsely:one_of([?DIGIT, ?LOWERCASE]),
 
-	Result = Parser("3a9"),
-	?assertEqual({ok, $3, <<"a9">>}, Result), 
+    Result = Parser("3a9"),
+    ?assertEqual({ok, $3, <<"a9">>}, Result),
 
-	Result2 = Parser("a69"),
-	?assertEqual({ok, $a, <<"69">>}, Result2), 
+    Result2 = Parser("a69"),
+    ?assertEqual({ok, $a, <<"69">>}, Result2),
 
-	assertError(Parser("Zaz")).
+    assertError(Parser("Zaz")).
 
 parse_one_of_none(_Config) ->
-	Parser = sparsely:one_of([]),
-	assertError(Parser("zaz")).
+    Parser = sparsely:one_of([]),
+    assertError(Parser("zaz")).
 
 parse_repeat(_Config) ->
-	Parser = sparsely:repeat(?DIGIT),
+    Parser = sparsely:repeat(?DIGIT),
 
-	Result = Parser("369"),
-	?assertEqual({ok, "369", <<>>}, Result), 
+    Result = Parser("369"),
+    ?assertEqual({ok, "369", <<>>}, Result),
 
-	Result2 = Parser("369999x6"),
-	?assertEqual({ok, "369999", <<"x6">>}, Result2), 
+    Result2 = Parser("369999x6"),
+    ?assertEqual({ok, "369999", <<"x6">>}, Result2),
 
-	assertError(Parser("zaz")).
+    assertError(Parser("zaz")).
 
 parse_chain(_Config) ->
-	Digit = ?DIGIT,
-	Lower = ?LOWERCASE,
-	Parser = sparsely:chain([Digit, Lower, Digit, Digit]),
+    Digit = ?DIGIT,
+    Lower = ?LOWERCASE,
+    Parser = sparsely:chain([Digit, Lower, Digit, Digit]),
 
-	Result = Parser("3x69"),
-	?assertEqual({ok, "3x69", <<>>}, Result), 
+    Result = Parser("3x69"),
+    ?assertEqual({ok, "3x69", <<>>}, Result),
 
-	Result2 = Parser("3x69999x6"),
-	?assertEqual({ok, "3x69", <<"999x6">>}, Result2), 
+    Result2 = Parser("3x69999x6"),
+    ?assertEqual({ok, "3x69", <<"999x6">>}, Result2),
 
-	assertError(Parser("3x6")),
-	assertError(Parser("x66zaz")).
+    assertError(Parser("3x6")),
+    assertError(Parser("x66zaz")).
 
 parse_optional(_Config) ->
-	Digit = ?DIGIT,
-	Lower = ?LOWERCASE,
-	Optional = sparsely:optional(Digit),
-	Parser = sparsely:chain([Optional, Lower, Digit, Digit]),
+    Digit = ?DIGIT,
+    Lower = ?LOWERCASE,
+    Optional = sparsely:optional(Digit),
+    Parser = sparsely:chain([Optional, Lower, Digit, Digit]),
 
-	Result = Parser("x69"),
-	?assertEqual({ok, "x69", <<>>}, Result), 
+    Result = Parser("x69"),
+    ?assertEqual({ok, "x69", <<>>}, Result),
 
-	Result2 = Parser("3x69"),
-	?assertEqual({ok, "3x69", <<>>}, Result2), 
+    Result2 = Parser("3x69"),
+    ?assertEqual({ok, "3x69", <<>>}, Result2),
 
-	Result3 = Parser("3x69999x6"),
-	?assertEqual({ok, "3x69", <<"999x6">>}, Result3), 
+    Result3 = Parser("3x69999x6"),
+    ?assertEqual({ok, "3x69", <<"999x6">>}, Result3),
 
-	Result4 = Parser("x69999x6"),
-	?assertEqual({ok, "x69", <<"999x6">>}, Result4), 
+    Result4 = Parser("x69999x6"),
+    ?assertEqual({ok, "x69", <<"999x6">>}, Result4),
 
-	assertError(Parser("3x6")),
-	assertError(Parser("X66zaz")).
+    assertError(Parser("3x6")),
+    assertError(Parser("X66zaz")).
 
 parse_skip(_Config) ->
-	Digit = ?DIGIT,
-	Lower = ?LOWERCASE,
-	Skip = sparsely:skip(Digit),
-	Parser = sparsely:chain([Skip, Lower, Digit, Digit]),
+    Digit = ?DIGIT,
+    Lower = ?LOWERCASE,
+    Skip = sparsely:skip(Digit),
+    Parser = sparsely:chain([Skip, Lower, Digit, Digit]),
 
-	Result = Parser("x69"),
-	?assertEqual({ok, "x69", <<>>}, Result), 
+    Result = Parser("x69"),
+    ?assertEqual({ok, "x69", <<>>}, Result),
 
-	Result2 = Parser("3x69"),
-	?assertEqual({ok, "x69", <<>>}, Result2).
+    Result2 = Parser("3x69"),
+    ?assertEqual({ok, "x69", <<>>}, Result2).
+
+parse_ignore(_Config) ->
+    Digit = ?DIGIT,
+    Lower = ?LOWERCASE,
+    Ignore = sparsely:ignore(Digit),
+    Parser = sparsely:chain([Ignore, Lower, Digit, Digit]),
+
+    assertError(Parser("x69")),
+
+    Result2 = Parser("3x69"),
+    ?assertEqual({ok, "x69", <<>>}, Result2).
 
 parse_optional_with_default(_Config) ->
-	Digit = ?DIGIT,
-	Lower = ?LOWERCASE,
-	Optional = sparsely:optional(Digit, $N),
-	Parser = sparsely:chain([Optional, Lower, Digit, Digit]),
+    Digit = ?DIGIT,
+    Lower = ?LOWERCASE,
+    Optional = sparsely:optional(Digit, $N),
+    Parser = sparsely:chain([Optional, Lower, Digit, Digit]),
 
-	Result = Parser("x69"),
-	?assertEqual({ok, "Nx69", <<>>}, Result), 
+    Result = Parser("x69"),
+    ?assertEqual({ok, "Nx69", <<>>}, Result),
 
-	Result2 = Parser("3x69"),
-	?assertEqual({ok, "3x69", <<>>}, Result2), 
+    Result2 = Parser("3x69"),
+    ?assertEqual({ok, "3x69", <<>>}, Result2),
 
-	Result3 = Parser("3x69999x6"),
-	?assertEqual({ok, "3x69", <<"999x6">>}, Result3), 
+    Result3 = Parser("3x69999x6"),
+    ?assertEqual({ok, "3x69", <<"999x6">>}, Result3),
 
-	Result4 = Parser("x69999x6"),
-	?assertEqual({ok, "Nx69", <<"999x6">>}, Result4), 
+    Result4 = Parser("x69999x6"),
+    ?assertEqual({ok, "Nx69", <<"999x6">>}, Result4),
 
-	assertError(Parser("3x6")),
-	assertError(Parser("X66zaz")).
+    assertError(Parser("3x6")),
+    assertError(Parser("X66zaz")).
 
 parse_optional_with_long_default(_Config) ->
-	Digit = ?DIGIT,
-	Lower = ?LOWERCASE,
-	Optional = sparsely:optional(Digit, "fred"),
-	Parser = sparsely:chain([Optional, Lower, Digit, Digit]),
+    Digit = ?DIGIT,
+    Lower = ?LOWERCASE,
+    Optional = sparsely:optional(Digit, "fred"),
+    Parser = sparsely:chain([Optional, Lower, Digit, Digit]),
 
-	Result = Parser("x69"),
-	?assertEqual({ok, ["fred", $x, $6, $9], <<>>}, Result).
+    Result = Parser("x69"),
+    ?assertEqual({ok, ["fred", $x, $6, $9], <<>>}, Result).
 
 parse_repeat_times(_Config) ->
-	Parser = sparsely:repeat(?DIGIT, 3),
+    Parser = sparsely:repeat(?DIGIT, 3),
 
-	Result = Parser("369"),
-	?assertEqual({ok, "369", <<>>}, Result), 
+    Result = Parser("369"),
+    ?assertEqual({ok, "369", <<>>}, Result),
 
-	Result2 = Parser("369999x6"),
-	?assertEqual({ok, "369", <<"999x6">>}, Result2), 
+    Result2 = Parser("369999x6"),
+    ?assertEqual({ok, "369", <<"999x6">>}, Result2),
 
-	assertError(Parser("66")),
-	assertError(Parser("66zaz")).
+    assertError(Parser("66")),
+    assertError(Parser("66zaz")).
 
 parse_match(_Config) ->
-	Parser = sparsely:match("1-800"),
+    Parser = sparsely:match("1-800"),
 
-	Result = Parser("1-800"),
-	?assertEqual({ok, "1-800", <<>>}, Result), 
+    Result = Parser("1-800"),
+    ?assertEqual({ok, "1-800", <<>>}, Result),
 
-	Result2 = Parser("1-800-555-1212"),
-	?assertEqual({ok, "1-800", <<"-555-1212">>}, Result2), 
+    Result2 = Parser("1-800-555-1212"),
+    ?assertEqual({ok, "1-800", <<"-555-1212">>}, Result2),
 
-	assertError(Parser("1-801-555-1212")),
-	assertError(Parser("1-80")).
+    assertError(Parser("1-801-555-1212")),
+    assertError(Parser("1-80")).
 
 parse_wrap(_Config) ->
-	Digit = ?DIGIT,
+    Digit = ?DIGIT,
 
-	Parser = sparsely:wrap(sparsely:repeat(Digit),
-				       fun({ok, Value, Rest}) -> {Integer, _} = string:to_integer(Value),
-								 {ok, Integer, Rest};
-					  (Any) -> Any
-				       end),
+    Parser =
+        sparsely:wrap(
+            sparsely:repeat(Digit),
+            fun ({ok, Value, Rest}) ->
+                    {Integer, _} = string:to_integer(Value),
+                    {ok, Integer, Rest};
+                (Any) ->
+                    Any
+            end),
 
-	Result = Parser("3456"),
-	?assertEqual({ok, 3456, <<>>}, Result), 
+    Result = Parser("3456"),
+    ?assertEqual({ok, 3456, <<>>}, Result),
 
-	Result2 = Parser("3456y"),
-	?assertEqual({ok, 3456, <<"y">>}, Result2), 
+    Result2 = Parser("3456y"),
+    ?assertEqual({ok, 3456, <<"y">>}, Result2),
 
-	assertError(Parser("x80")).
+    assertError(Parser("x80")).
 
 one_digit(S) ->
-	Parser = ?DIGIT,
-	Parser(S).
+    Parser = ?DIGIT,
+    Parser(S).
 
 parse_fun(_Config) ->
-	Parser = sparsely:parse_fun(sparsely_SUITE, one_digit),
-	Result = Parser("3456"),
-	?assertEqual({ok, $3, <<"456">>}, Result).
-
-
-
-
+    Parser = sparsely:parse_fun(sparsely_SUITE, one_digit),
+    Result = Parser("3456"),
+    ?assertEqual({ok, $3, <<"456">>}, Result).
